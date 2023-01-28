49f7d13b11c67e7421a5c0b5bcd5c3fb63d66fab
dmendenhall <113963164+dmendenhall@users.noreply.github.com>
2023-01-28 00:41:55 -0700

    Initial commit

---
+++ chamber/scripts/current_date.js
@@ -0,0 +1,8 @@
+const datefieldUK = document.querySelector("aside");
+
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
